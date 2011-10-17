8d118211f2d3e26f7df4637fd305ce0739449d6a
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-10-17 14:46:34 +0200

    preprare interface for options

---
+++ collective/aviaryimageeditor/i18n.py
@@ -0,0 +1,14 @@
+from collective.aviaryimageeditor import messageFactory as _
+
+desc_all = _(u"Using this option will mean any new features that Aviary \
+  releases will automatically appear without needing to update your code. \
+  This option is recommended.")
+
+desc_NoCloseButton = _(u"Set to ‘true’ to remove the close button. This will \
+  only affect the inject OpenType version of the editor.")
+
+desc_Stickers = _(u"An array of sticker urls in the following format:\
+  ['http://big_image, http://thumbnail_image',...]")
+
+desc_CropSizes = _(u"A space separated list of preset crop sizes for crop tool.\
+  Width and Height are separated by commas.")
